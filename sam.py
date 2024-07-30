706b227d6fda9ac8c99491828c7cf9311cc46333
SAHIL A GOWDA <sahilgowda204@gmail.com>
2024-07-30 20:41:07 +0530

    Updated the frontend
    Added all the feature as described

--- sam.py
+++ sam.py
@@ -2,62 +2,62 @@ import gradio as gr
 from hugchat import hugchat
 from hugchat.login import Login
 
-EMAIL = "name of your email"
-PASSWD = "your password"
+EMAIL = "sahilgowda204@gmail.com"
+PASSWD = "Sahilgowda2004"
 cookie_path_dir = "./cookies/"  
 sign = Login(EMAIL, PASSWD)
 cookies = sign.login(cookie_dir_path=cookie_path_dir, save_cookies=True)
-
 chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
 
 def chat_function(message):
     response = chatbot.chat(message).wait_until_done()
     return response
 
-def upload_function(file):
-    return f"Uploaded: {file.name}"
+def generate_image(prompt):
+   
+    return "Image would be generated based on: " + prompt
 
-
-with gr.Blocks() as demo:
+with gr.Blocks(theme=gr.themes.Soft()) as demo:
     with gr.Row():
-       
+        # Left column for chat
         with gr.Column(scale=1):
             gr.Markdown("## Chat Box")
             chatbox = gr.Chatbot(height=600)
             chat_input = gr.Textbox(show_label=False, placeholder="Type a message...")
             chat_button = gr.Button("Send")
 
-            def update_chat(chatbox, message):                
-                chatbox.append(("User", message))                
-                response = chat_function(message)
-                chatbox.append(("Bot", response))
-                return chatbox
-
-            chat_button.click(update_chat, [chatbox, chat_input], chatbox)
-
-        
-        with gr.Column(scale=1):
-            gr.Markdown("## Generate Image")
+       
+        with gr.Column(scale=2):
+            gr.Markdown("## Image Generation and Analysis")
+            
             
             with gr.Row():
-                with gr.Column():
-                    gr.Markdown("### Upload File")
-                    file_input = gr.File()
+                with gr.Column(scale=1):
+                    upload_box = gr.Image(label="Upload Image", type="pil", height=400,width=500)
                     upload_button = gr.Button("Upload")
-                    suggestion = gr.Textbox(placeholder="Ask how to improve the image...")
-                    # upload_response = gr.Textbox()
-                    chat_response = gr.Textbox(placeholder="Suggestion how to Improve the image")
-                    
-                    
-        
+                with gr.Column(scale=1):
+                    prompt_input = gr.Textbox(placeholder="Enter the prompt for image generation ...", label="Prompt")
+                    generated_box = gr.Image(label="Generated Image", type="pil", height=300)
+                    generate_button = gr.Button("Generate Image", variant="primary", size="sm")
 
-                    # def handle_upload(file, suggestion_text):
-                    #     # Handle file upload and provide response
-                    #     file_response = upload_function(file)
-                    #     upload_response = gr.Textbox()
-                    #     chat_response = chat_function(suggestion_text)
-                    #     return file_response, chat_response, upload_response
-                    
-                    # upload_button.click(handle_upload, [file_input, suggestion], [upload_response, chat_response])
+            
+            graph_output = gr.Plot(label="Analysis Graph")
+
+    def update_chat(chatbox, message):                
+        chatbox.append(("User", message))                
+        response = chat_function(message)
+        chatbox.append(("Bot", response))
+        return chatbox
+
+    def handle_image_generation(prompt):
+        
+        return generate_image(prompt)
+
+    def handle_upload(image):
+        return image
+
+    chat_button.click(update_chat, [chatbox, chat_input], chatbox)
+    generate_button.click(handle_image_generation, prompt_input, generated_box)
+    upload_button.click(handle_upload, upload_box, upload_box)
 
 demo.launch()
